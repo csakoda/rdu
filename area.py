49194104f8f82690ccb001cccbbbf2eb7e4e60db
littlegustv <echo29x@gmail.com>
2014-11-17 19:40:22 -0500

    In progress modifying AREA files, and the code that loads them.

--- area.py
+++ area.py
@@ -13,52 +13,59 @@ class Area():
 
 def initialize_area():
     import copy
+    import os
 
-    f = open("data/areas/cantina.are")
-    temp_area = Area("cantina")
-    _.areas.append(temp_area)
-    lines = f.readlines()
-    temp_room = None
-    for l in lines:
-        if l == "--- START ROOM ---\n":
-            temp_room = room.Room("", temp_area, "", "")
-            temp_mobiles = []
-            continue
-        elif l == "---  END ROOM  ---\n":
-            for m in temp_mobiles:
-                m.stats["room"] = temp_room.vnum
-                _.mobiles.append(m)
-            _.rooms.append(temp_room)
-            temp_area.rooms.append(temp_room)
-            temp_room = None
-            continue
-        try:
-            temp_key = l.split(":^:")[0].strip()
-            temp_value = l.split(":^:")[1].strip()
-            if temp_key == "vnum":
-                temp_room.vnum = temp_value
-            elif temp_key == "exit":
-                try:
-                    temp_vnum = l.split(":^:")[2].strip()
-                    temp_room.exits[int(temp_value)] = temp_vnum
-                except IndexError:
-                    print("Illegal exit found.")
-            elif temp_key == "mobiles":
-                for m in temp_value.split():
-                    available_mobs = [x for x in _.master_mobile_list if x.vnum == m]
-                    if len(available_mobs) < 1:
-                        print("Room reset contains unknown vnum \"%s\"." % m)
-                    elif len(available_mobs) > 1:
-                        print("Room reset contains duplicated vnum \"%s\"." % m)
-                    else:
-                        temp_mobiles.append(copy.deepcopy(available_mobs[0]))
-            else:
-                if temp_key == "desc":
-                    temp_value += "\n\r"
-                temp_room.stats[temp_key] = temp_value
-        except IndexError:
-            if ":^:" not in l: # This is a multi-line which for now can only be a description
-                temp_room.stats["desc"] += l.strip() + "\n\r"
-            else:
-                temp_value = ""
-                temp_room.stats[temp_key] = temp_value
+    areaFiles = []
+    for file in os.listdir("./data/areas"):
+        if file.endswith(".are"):
+            #print(file)
+            areaFiles.append(file)
+    for fn in areaFiles:
+        f = open("./data/areas/" + fn)
+        temp_area = Area(fn[:len(fn) - 4])
+        _.areas.append(temp_area)
+        lines = f.readlines()
+        temp_room = None
+        for l in lines:
+            if l == "--- START ROOM ---\n":
+                temp_room = room.Room("", temp_area, "", "")
+                temp_mobiles = []
+                continue
+            elif l == "---  END ROOM  ---\n":
+                for m in temp_mobiles:
+                    m.stats["room"] = temp_room.vnum
+                    _.mobiles.append(m)
+                _.rooms.append(temp_room)
+                temp_area.rooms.append(temp_room)
+                temp_room = None
+                continue
+            try:
+                temp_key = l.split(":^:")[0].strip()
+                temp_value = l.split(":^:")[1].strip()
+                if temp_key == "vnum":
+                    temp_room.vnum = temp_value
+                elif temp_key == "exit":
+                    try:
+                        temp_vnum = l.split(":^:")[2].strip()
+                        temp_room.exits[int(temp_value)] = temp_vnum
+                    except IndexError:
+                        print("Illegal exit found.")
+                elif temp_key == "mobiles":
+                    for m in temp_value.split():
+                        available_mobs = [x for x in _.master_mobile_list if x.vnum == m]
+                        if len(available_mobs) < 1:
+                            print("Room reset contains unknown vnum \"%s\"." % m)
+                        elif len(available_mobs) > 1:
+                            print("Room reset contains duplicated vnum \"%s\"." % m)
+                        else:
+                            temp_mobiles.append(copy.deepcopy(available_mobs[0]))
+                else:
+                    if temp_key == "desc":
+                        temp_value += "\n\r"
+                    temp_room.stats[temp_key] = temp_value
+            except IndexError:
+                if ":^:" not in l: # This is a multi-line which for now can only be a description
+                    temp_room.stats["desc"] += l.strip() + "\n\r"
+                else:
+                    temp_value = ""
+                    temp_room.stats[temp_key] = temp_value
